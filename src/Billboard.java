0d104e748fb3879509467a12ea4756eb2121674d
lukecheng1998 <lukecheng1998@gmail.com>
2019-10-05 02:21:54 -0400

    Some progress with this

--- src/Billboard.java
+++ src/Billboard.java
@@ -1,7 +1,14 @@
-import java.awt.Canvas;
-import java.awt.Graphics;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
-public class Billboard {
+public class Billboard extends JComponent {
+
+    public void paintComponent(Graphics g) {
+        if(g instanceof Graphics2D){
+            Graphics2D g2 = (Graphics2D)g;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.drawString("Hello World", 70, 80);
+        }
+    }
 
 }
